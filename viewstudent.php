6efeacf8a54e507ded3ae2a467ba76956b9de0a8
azharbinsagar <azhar.binsagar@yahoo.com>
2022-03-03 15:03:54 +0530

    first commit

---
+++ viewstudent.php
@@ -0,0 +1,50 @@
+<?php
+require_once("db.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Student</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Mobile</th>
+      <th scope="col">Email</th>
+      <th scope="col">Course</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    <?php 
+    //taking data from database
+        $query = "SELECT * FROM `student` ORDER BY `name`";
+		$result = mysqli_query($con,$query) or die(mysqli_error($con));
+		$c=1;
+		while($obj=mysqli_fetch_object($result))
+		{	
+			echo "<tr> <td>$c</td>";
+            echo"
+                <td>$obj->name</td>
+                <td>$obj->mob</td>
+                <td>$obj->email</td>
+                <td>$obj->course</td>";
+			$c++;
+		}
+    ?>
+
+  </tbody>
+</table>
+</body>
+</html>
